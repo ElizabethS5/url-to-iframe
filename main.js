f4bc4f44529b27f8e2a88f43d55e3e4a17ccf33e
Elizabeth5 <escheidt5@gmail.com>
2022-03-02 19:04:36 -0500

    adds some styling and message when saving domain

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
-const deploymentInput = document.getElementById("deployment");
+const domainInput = document.getElementById("domain");
 const urlInput = document.getElementById("page-url");
 const clearButton = document.getElementById("clear");
-const saveDeploymentButton = document.getElementById("save");
+const savedomainButton = document.getElementById("save");
+const messageDiv = document.getElementById("message");
 const regex = /(\/modules\S+)/;
 
-let defaultDeploymentString = deploymentInput.value;
-let localDeploymentString = localStorage.getItem("deployment");
-console.log(localDeploymentString);
-let deploymentString;
-if (localDeploymentString === null) {
-  localStorage.setItem("deployment", defaultDeploymentString);
-  deploymentString = defaultDeploymentString;
+let defaultdomainString = domainInput.value;
+let localdomainString = localStorage.getItem("domain");
+let domainString;
+if (localdomainString === null) {
+  localStorage.setItem("domain", defaultdomainString);
+  domainString = defaultdomainString;
 } else {
-  deploymentString = localDeploymentString;
-  deploymentInput.value = localDeploymentString;
+  domainString = localdomainString;
+  domainInput.value = localdomainString;
 }
 let urlString = "";
 
@@ -23,13 +23,13 @@ urlInput.addEventListener("input", (e) => {
   if (paths === null) return;
   let path = paths[0];
   let value =
-    `<p><iframe style="width: 100%; height: 1024px" src="https://${deploymentString}${path}" width="100%" height="1024px" allowfullscreen="allowfullscreen" webkitallowfullscreen="webkitallowfullscreen" mozallowfullscreen="mozallowfullscreen"></iframe></p>`.toString();
+    `<p><iframe style="width: 100%; height: 1024px" src="https://${domainString}${path}" width="100%" height="1024px" allowfullscreen="allowfullscreen" webkitallowfullscreen="webkitallowfullscreen" mozallowfullscreen="mozallowfullscreen"></iframe></p>`.toString();
   navigator.clipboard.writeText(value);
-  console.log("copied to clipboard\n", value);
+  showMessage("iframe copied to clipboard");
 });
 
-deploymentInput.addEventListener("input", (e) => {
-  deploymentString = deploymentInput.value.trim();
+domainInput.addEventListener("input", (e) => {
+  domainString = domainInput.value.trim();
 });
 
 clearButton.addEventListener("click", (e) => {
@@ -37,7 +37,21 @@ clearButton.addEventListener("click", (e) => {
   urlInput.value = "";
 });
 
-saveDeploymentButton.addEventListener("click", (e) => {
-  deploymentString = deploymentInput.value.trim();
-  localStorage.setItem("deployment", deploymentString);
+savedomainButton.addEventListener("click", (e) => {
+  domainString = domainInput.value.trim();
+  localStorage.setItem("domain", domainString);
+  showMessage("domain saved to local storage");
 });
+
+function showMessage(message) {
+  messageDiv.innerText = message;
+  messageDiv.classList.remove("fade-out");
+  messageDiv.classList.add("fade-in");
+  setTimeout((e) => {
+    messageDiv.classList.remove("fade-in");
+    messageDiv.classList.add("fade-out");
+  }, 1500);
+  setTimeout((e) => {
+    messageDiv.innerText = "";
+  }, 3000);
+}
